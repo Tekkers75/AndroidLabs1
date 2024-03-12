024e90e8d623ee8c763328ffcbf1f3030c6af94a
Tekkers75 <Tekkers75@gmail.com>
2024-03-12 23:36:27 +0900

    2lab

--- 2Lab/app/src/main/java/com/example/myapp/MainActivity.java
+++ 2Lab/app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button button1;
     TextView TextView1;
     EditText EditText1;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         button1 = findViewById(R.id.button);
         TextView1 = findViewById(R.id.Text1);
         EditText1 = findViewById(R.id.editTextText);
+        button2 = findViewById(R.id.button);
 
 
     }
@@ -33,4 +36,10 @@ public class MainActivity extends AppCompatActivity {
         TextView1.setText(EditText1.getText().toString());
     }
     ///
+    public void onMyButtonClick1(View view)
+    {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
 }
